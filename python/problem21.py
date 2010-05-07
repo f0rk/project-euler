42cb046558cb4f31435fe3cfb59887f52b7e2b5f
Ryan P. Kelly <rpkelly22@gmail.com>
2010-05-06 20:14:40 -0400

    Solutions to problems 21 and 22, plus associated files for those and other problems

---
+++ python/problem21.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+#solution: 
+
+import math
+
+def proper_divisors(n):
+    divs = [1]
+    i = 2
+    while i < math.sqrt(n):
+        if n % i == 0:
+            divs.append(i)
+            divs.append(n / i)
+        i += 1
+    return divs
+
+def euler_problem_21():
+    totalsum = 0
+    for i in xrange(2, 10000):
+        divsum = sum(proper_divisors(i))
+        othersum = sum(proper_divisors(divsum))
+        if i == othersum and i != divsum:
+            totalsum += i
+            totalsum += divsum
+    return totalsum / 2 # everything gets added twice
+
+print euler_problem_21()
---
+++ python/problem22.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#solution: 
+
+def name_score(name):
+    total = 0
+    for i in range(len(name)):
+        total += ord(name[i]) - ord("A") + 1
+    return total
+
+def euler_problem_22():
+    name_file = open("problem22.txt", "r")
+    names = name_file.read()
+    names = names.strip('"')
+    name_list = names.split('","')
+    name_list.sort()
+    total = 0
+    for i in range(len(name_list)):
+        score = name_score(name_list[i])
+        total += score * (i + 1)
+    return total
+
+print euler_problem_22()
